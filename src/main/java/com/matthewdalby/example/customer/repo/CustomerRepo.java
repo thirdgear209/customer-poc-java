f696f5130a83f09626ff83d0ee60d2baa1e667e0
matthew dalby <matthew.dalby@g3enterprises.com>
2024-08-26 12:19:37 -0700

    Initial project

--- src/main/java/com/matthewdalby/example/customer/repo/CustomerRepo.java
+++ src/main/java/com/matthewdalby/example/customer/repo/CustomerRepo.java
@@ -1,11 +1,13 @@
 package com.matthewdalby.example.customer.repo;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.matthewdalby.example.customer.entity.Customer;
+import com.matthewdalby.example.customer.vo.CustomerRefVo;
 
 public interface CustomerRepo extends CrudRepository<Customer, Long>{
 
@@ -16,5 +18,8 @@ public interface CustomerRepo extends CrudRepository<Customer, Long>{
 	@Query(nativeQuery=true, value="select customer.id from Customer customer where customer.customer_number = :customerNumber")
 	public Long getCustomerIdByCustomerNumber(String customerNumber);
 	
+	@Query(nativeQuery=true, value="select customer.*, phone_number.number from customer inner join phone_number where phone_number.customer_id = customer.id and customer.first_name like '%' and customer.last_name like '%' \n"
+			+ "and city = ")
+	public List<CustomerRefVo> customerSearch (String firstName, String lastName, String city);
 	
 }
